d3f4dc9c376f25fabf338cdb6e92b1eeb2fdf450
Dionys Steffen <steffen@seventy.mx>
2023-02-08 10:11:10 +0100

    *CatchallController add Scripts and Styles to viewModel

--- API/Controllers/CatchallController.cs
+++ API/Controllers/CatchallController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.IO;
@@ -18,35 +19,63 @@ namespace API.Controllers
             _environment = environment;
         }
 
-        // GET: /Catchall/
+        public class FileType
+        {
+            public static readonly (string, string) JavaScript = ("Scripts", "js");
+            public static readonly (string, string) Stylesheet = ("Styles", "css");
+
+            private readonly (string, string) _fileType;
+            
+            public string Folder => _fileType.Item1;
+            public string FileExtension => _fileType.Item2;
+
+            public FileType((string, string) fileType)
+            {
+                _fileType = fileType;
+            }
+        }
+
+        private IList<string> GetFiles(string viewName, FileType fileType)
+        {
+            viewName = viewName.ToLower();
+
+            var reg1 = new Regex(@"^_");
+            var reg2 = new Regex($"{viewName}");
+
+            var file = $@"{_environment.WebRootPath}\{fileType.Folder}\{viewName}.{fileType.FileExtension}";
+            var fileExists = Exists(file);
+
+            var fileList = Directory.GetFiles($@"{_environment.WebRootPath}\{fileType.Folder}\", $"*.{fileType.FileExtension}")
+                .Select(Path.GetFileName)
+                .Where(n => n.Contains('_'))
+                .Where(n => reg1.IsMatch(n) || reg2.IsMatch(n))
+                .ToList();
+
+            if (fileExists)
+                fileList.Add($"{viewName}.{fileType.FileExtension}");
+
+            return fileList;
+        }
+
+            // GET: /Catchall/
         [AllowAnonymous]
         public ActionResult Index(string name = "Index")
         {
             var file = $@"Views\{name}.cshtml";
             var viewExists = Exists(file);
 
-            var script = $@"{_environment.WebRootPath}\Scripts\{name}.js";
-            var scriptExists = Exists(script);
-
             // check if view name requested is not found
             if (!viewExists)
                 return NotFound();
 
-            var reg1 = new Regex(@"^_");
-            var reg2 = new Regex($"{name}");
-
-            var scriptList = Directory.GetFiles($@"{_environment.WebRootPath}\Scripts\", "*.js")
-                .Select(Path.GetFileName)
-                .Where(n => n.Contains('_'))
-                .Where(n => reg1.IsMatch(n) || reg2.IsMatch(n))
-                .ToList();
-
-            if (scriptExists)
-                scriptList.Add($"{name}.js");
-
             // otherwise just return the view
-            var viewDto = new { Title = name, Scripts = scriptList };
-            return View(file, viewDto);
+            var viewModel = new
+            {
+                Title = name,
+                Scripts = GetFiles(name, new FileType(FileType.JavaScript)),
+                Styles = GetFiles(name, new FileType(FileType.Stylesheet)),
+            };
+            return View(file, viewModel);
         }
     }
 }
--- API/wwwroot/Scripts/_MainScript.js
+++
@@ -1,1 +0,0 @@
-alert("Main Script says Hello!")
